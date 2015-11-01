0fc13af7b5f3f67479f5d8a4ccfbe043f28d718a
JoshFerguson <josh@inspirosity.net>
2015-11-01 00:05:59 -0500

    SQL UPDATE IMPORTANT

--- PccBay/includes/sql/PccBay.sql
+++ PccBay/includes/sql/PccBay.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 28, 2015 at 05:39 PM
+-- Generation Time: Nov 01, 2015 at 06:05 AM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -33,7 +33,15 @@ CREATE TABLE IF NOT EXISTS `pb_comments` (
   `author` varchar(200) NOT NULL,
   `status` varchar(200) NOT NULL,
   `comment` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pb_comments`
+--
+
+INSERT INTO `pb_comments` (`id`, `post_id`, `date`, `author`, `status`, `comment`) VALUES
+(1, '10', 'October 31, 2015, 11:40 pm', '100006044469574', 'open', 'Can you budge on the price at all?'),
+(2, '10', 'October 31, 2015, 11:41 pm', '10000607254332', 'open', 'Nah this is a good price, Ill Take it!!');
 
 -- --------------------------------------------------------
 
@@ -48,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `pb_post` (
   `product_info` longtext NOT NULL COMMENT 'Product title, description, price, date created',
   `trans_info` varchar(1024) NOT NULL COMMENT 'sold to, date sold, payment method, transaction complete flag',
   `status` varchar(200) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pb_post`
@@ -63,7 +71,8 @@ INSERT INTO `pb_post` (`product_id`, `type`, `user_id`, `product_info`, `trans_i
 (6, 'product', '100001056120276', '[{"timestamp":"October 26, 2015, 6:36 pm","title":"K-cups","desc":"","tags":"K-cups,coffee,drink","price":"20.00","condition":"91","images":"1A0kP9T75X"}]', '[{"completed":0,"method":0,"sold_to":0,"date_sold":0}]', 'open'),
 (7, 'question', '100006044469574', '[{"timestamp":"October 27, 2015, 12:01 am","title":"How to use PCCbay","desc":"Does anyone know how to use this?","tags":"FAQ,PCCbay,help","price":"","condition":"null","images":""}]', '[{"completed":0,"method":0,"sold_to":0,"date_sold":0}]', 'open'),
 (8, 'discussion', '100006044469574', '[{"timestamp":"October 27, 2015, 12:15 am","title":"Fine Arts","desc":"How About that child Prodigy","tags":"Prodigy,Fine Arts,Ethan Bortnick","price":"","condition":"null","images":"G6gbX9ICnT"}]', '[{"completed":0,"method":0,"sold_to":0,"date_sold":0}]', 'open'),
-(9, 'discussion', '100001056120276', '[{"timestamp":"October 27, 2015, 8:53 pm","title":"The Big Idea Behind PCCbay","desc":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis vehicula eros in dapibus. Nunc mattis neque porttitor tempor congue. Pellentesque convallis feugiat pretium. Donec maximus imperdiet ligula efficitur convallis. Proin hendrerit semper augue,","tags":"Lorem,ipsum,PCCbay","price":"","condition":"null","images":""}]', '[{"completed":0,"method":0,"sold_to":0,"date_sold":0}]', 'open');
+(9, 'discussion', '100001056120276', '[{"timestamp":"October 27, 2015, 8:53 pm","title":"The Big Idea Behind PCCbay","desc":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis vehicula eros in dapibus. Nunc mattis neque porttitor tempor congue. Pellentesque convallis feugiat pretium. Donec maximus imperdiet ligula efficitur convallis. Proin hendrerit semper augue,","tags":"Lorem,ipsum,PCCbay","price":"","condition":"null","images":""}]', '[{"completed":0,"method":0,"sold_to":0,"date_sold":0}]', 'open'),
+(10, 'product', '100006044469574', '[{"timestamp":"October 31, 2015, 3:35 am","title":"2015 MacBook Pro with Retina display","desc":"<ol><li>2.7GHz Processor</li><li>2.7GHz dual-core Intel Core</li><li>i5 Turbo Boost up to 3.1GHz</li><li>8GB 1866MHz LPDDR3 memory 128GB PCIe-based flash storage</li></ol><ul><li>Intel Iris Graphics 6100</li><li>Built-in battery (10 hours)</li><li>2 Force Touch trackpad</li><li>Power adapter and power cord</li><li>AC Wall Plug</li></ul><p>A Mac includes 90 days of free telephone technical support and a one-year limited warranty. If you purchase the AppleCare Protection Plan, you can extend that coverage to three years from the original purchase date of your Mac 128 GB Storage</p>","tags":"MacBook Pro,Laptop,Computer,Mac,Apple","price":"1,299.00","condition":"80","images":"3dhALzY0OI,pBAXPpOsOR,9dbJWlQABt"}]', '[{"completed":0,"method":0,"sold_to":0,"date_sold":0}]', 'open');
 
 -- --------------------------------------------------------
 
@@ -80,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `pb_safe_image` (
   `author` varchar(200) NOT NULL,
   `file` varchar(200) NOT NULL,
   `string` longtext NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pb_safe_image`
@@ -97,7 +106,11 @@ INSERT INTO `pb_safe_image` (`id`, `uid`, `size`, `type`, `date`, `author`, `fil
 (8, '64V78HQp0V', '644:568', 'jpg', 'October 26, 2015, 6:32 pm', '100006044469574', '27e14a539a3db86f261a670ba5cd9044AA50jKF2tg.jpg', '/images/user-data/2015_26_10/27e14a539a3db86f261a670ba5cd9044AA50jKF2tg.jpg'),
 (9, 'NfAsvbVKJn', '480:853', 'jpg', 'October 26, 2015, 6:34 pm', '100006044469574', 'd15bddd1c93a813bf605e17acdbbfa36Z5iPPwi06S.jpg', '/images/user-data/2015_26_10/d15bddd1c93a813bf605e17acdbbfa36Z5iPPwi06S.jpg'),
 (10, '1A0kP9T75X', '960:540', 'jpg', 'October 26, 2015, 6:36 pm', '100006044469574', '8bce111b8fa015cd09176343e9fb6996ixz8sFeCFM.jpg', '/images/user-data/2015_26_10/8bce111b8fa015cd09176343e9fb6996ixz8sFeCFM.jpg'),
-(13, 'G6gbX9ICnT', '534:401', 'jpg', 'October 27, 2015, 12:15 am', '100006044469574', 'cc1e682e2a7b3ff5fba509c77effffb99362baa9.jpg', '/images/user-data/2015_10_27/cc1e682e2a7b3ff5fba509c77effffb99362baa9.jpg');
+(13, 'G6gbX9ICnT', '534:401', 'jpg', 'October 27, 2015, 12:15 am', '100006044469574', 'cc1e682e2a7b3ff5fba509c77effffb99362baa9.jpg', '/images/user-data/2015_10_27/cc1e682e2a7b3ff5fba509c77effffb99362baa9.jpg'),
+(14, '3dhALzY0OI', '769:517', 'jpg', 'October 31, 2015, 3:35 am', '100006044469574', '52ca1ef1dfacb161a8f23998938da9e010ef9ce0.jpg', '/images/user-data/2015_10_31/52ca1ef1dfacb161a8f23998938da9e010ef9ce0.jpg'),
+(15, 'pBAXPpOsOR', '900:530', 'jpg', 'October 31, 2015, 3:35 am', '100006044469574', '197a50d9c6a1f915025561bda761ad1f7af4abf6.jpg', '/images/user-data/2015_10_31/197a50d9c6a1f915025561bda761ad1f7af4abf6.jpg'),
+(16, '9dbJWlQABt', '534:312', 'png', 'October 31, 2015, 3:35 am', '100006044469574', '7bcf2c52e082f824b7f6067ae5eeddd936cbe953.png', '/images/user-data/2015_10_31/7bcf2c52e082f824b7f6067ae5eeddd936cbe953.png'),
+(17, '57BXJb3A7W', '235:235', 'jpg', 'October 31, 2015, 3:41 am', '100006044469574', 'a8c99d09a3cd14b42bc7b7bb47a203a427d64e30.jpg', '/images/user-data/2015_10_31/a8c99d09a3cd14b42bc7b7bb47a203a427d64e30.jpg');
 
 -- --------------------------------------------------------
 
@@ -142,7 +155,7 @@ CREATE TABLE IF NOT EXISTS `pb_users` (
 INSERT INTO `pb_users` (`user_id`, `num_of_ratings`, `total_ratings`, `permissions`, `id_card_key`, `contact_info`, `user_data`) VALUES
 ('100006044469574', 0, 0, 100, '124981', '[{"resident":"false","building":"","room":"","phone":"850 281-9161","email":"josh@inspirosity.net","notify_d":"true","notify_m":"true"}]', '[{"ID":"100006044469574","username":"JoshFerguson","name":"Josh Ferguson","avatar":"https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpl1/t31.0-8/11228039_1686546648223468_7048846777877974576_o.jpg","registered":"10/20/2015","permissions":"100","theme":"dark","interest":"Design,Graphics,Advertising,Textbooks,Pensacola FL"}]'),
 ('100001056120276', 0, 0, 100, '', '[{"resident":"true","building":"","room":"","phone":"","email":"","notify_d":"true","notify_m":"true"}]', '[{"ID":"100001056120276","username":"joseph.gengarella","name":"Joseph Gengarella","avatar":"https://scontent-iad3-1.xx.fbcdn.net/hphotos-xlp1/v/t1.0-9/10403522_878791748832688_1007985923560409411_n.jpg?oh=08d6efbe0cbea75280afdf0895ede224&oe=56B3F054","registered":"10/27/2015","permissions":"100","theme":"default","interest":""}]'),
-('10000607254332', 0, 0, 0, '000000', '[{"resident":"true","building":"Young","room":"345","phone":"(555) 555-5555","email":"TestUser@email.com","notify_d":"false","notify_m":"false"}]', '[{"ID":"10000607254332","username":"TestUser","name":"test User","avatar":"https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.nuclemed.com.br%2Fimages%2Fuser.png&f=1","registered":"10/01/2015","permissions":"100","theme":"blue","interest":""}]');
+('10000607254332', 0, 0, 0, '000000', '[{"resident":"true","building":"Young","room":"345","phone":"(555) 555-5555","email":"TestUser@email.com","notify_d":"false","notify_m":"false"}]', '[{"ID":"10000607254332","username":"SteveJobs","name":"Steve Jobs","avatar":"https://image.freepik.com/free-vector/steve-jobs-vector-tribute_25-14787.jpg","registered":"10/01/2015","permissions":"100","theme":"blue","interest":""}]');
 
 -- --------------------------------------------------------
 
@@ -212,17 +225,17 @@ ALTER TABLE `pb_user_points`
 -- AUTO_INCREMENT for table `pb_comments`
 --
 ALTER TABLE `pb_comments`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `pb_post`
 --
 ALTER TABLE `pb_post`
-  MODIFY `product_id` int(200) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `product_id` int(200) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `pb_safe_image`
 --
 ALTER TABLE `pb_safe_image`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `pb_short_url`
 --
