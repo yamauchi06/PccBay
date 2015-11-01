090c6bd1ff2230ba24616f40ee895c876cda2431
JoshFerguson <josh@inspirosity.net>
2015-10-31 23:55:45 -0500

    404 page and more with comments

---
+++ PccBay/404.php
@@ -0,0 +1,70 @@
+<?php  include_once($_SERVER['DOCUMENT_ROOT'].'/MasterPages/overhead.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>PCCbay | The eBay for PCC</title>
+		<?php pb_include('/MasterPages/head.php'); ?>
+		<style>
+		.Oldman{
+			background-image: url('/images/interior-images/404-oldman.png');
+			background-size: contain;
+			background-repeat: no-repeat;
+			background-position: left center;
+			padding: 150px;
+			margin-top: 50px;
+		}	
+		.Oldman h1{
+			font-size: 100px;
+		}
+		.Oldman p{
+			font-size: 23px;
+		}
+		small{
+			font-size: 16px;
+		}
+		footer{
+			position: absolute !important;
+			bottom: 0px;
+			left: 0px;
+			width: 100% !important;
+		}
+		</style>
+	</head>	
+<body>
+	<header>		
+		<div class="row">	
+			<div class="col-md-3">
+				<a href="/"><img src="/images/favicon/pccBay-logo.svg" height="40px" alt="PccBay" /></a>
+			</div>	
+			<div class="col-md-2 col-md-offset-3" id="headerBtns"></div>
+			<div class="col-md-4">
+			    <div class="input-group MainSearchBox transition-300">
+			      <input type="text" class="form-control" placeholder="Search PCCbay">
+			      <span class="input-group-btn">
+			        <button class="btn" type="button"> <span class="glyphicon glyphicon-search"></span> </button>
+			      </span>
+			    </div>
+			</div>
+		</div>
+	</header>
+	
+	<!-- Begin Content -->
+	<div class="Oldman"></div>
+<?php pb_include('/MasterPages/footer.php~col-md-12'); ?>
+<script>
+$(document).ready(function(){
+	$('.Oldman').css({
+		width: $(window).width(),
+		height: $(window).height()-50
+	});
+});
+$(window).resize(function(){
+	$('.Oldman').css({
+		width: $(window).width(),
+		height: $(window).height()-50
+	});
+});
+
+</script>
+</body>
+</html>
--- PccBay/product.php
+++ PccBay/product.php
@@ -39,7 +39,9 @@
 			$pb_user['theme']=$data['theme'];
 		}
 	}else{
-		header('Location: /404');
+		header('HTTP/1.0 404 Not Found');
+	    include('404.php');
+	    exit();
 	}	
 ?>
 <!DOCTYPE html>
@@ -61,7 +63,7 @@
 			<div class="col-md-9 MainFeed">
 
 				<?php
-					if(!empty($images)){
+					if(!empty($product['images'])){
 						?>
 						<div class="col-md-5">
 							<div class="pb-product-gallery">
@@ -149,7 +151,7 @@
 							<button class="pb-addtocart themeBG">Get This</button>
 						</form>
 						
-						<form action="" method="post">
+						<form action="" method="post" class="pb-comment-form">
 							<div class="pb-comment-area">
 								<input type="hidden" name="post_id" value="<?php print $product_id; ?>">
 								<textarea name="comment" placeholder="Leave a comment" class="fixedHeight autosize"></textarea>
@@ -212,6 +214,7 @@
 	</div>
 
 <script src="/includes/js/pb-product-slider.js"></script>
+<script src="/includes/js/pb-comments.js"></script>
 <?php pb_include('/MasterPages/footer.php'); ?>
 <script>
 $( window ).resize(function() {
@@ -227,80 +230,7 @@ $(document).ready(function(){
 		$('.pb-product-gallery').find('img:eq(0)').attr('src', $(this).attr('src'));
 	});
 	
-	$('body').on('click', '.pb-comment-area', function(event){
-		event.stopPropagation();
-		var commenter = $(this).find('textarea');
-		commenter.removeClass('fixedHeight').attr('pos', 'open').next('.pb-comment-area-lower').show(200);
-	});
-	$('body').on('click', function(){
-		var commenter = $('.pb-comment-area textarea');
-		if( commenter.attr('pos')=='open' ){
-			commenter.addClass('fixedHeight').attr('pos', 'closed').next('.pb-comment-area-lower').hide(400);
-		}
-	});
-	
-	$('body').on('click', '[name="add_comment"]', function(event){
-		event.preventDefault();
-		var textarea=$('.pb-comment-area textarea')
-		var comment = textarea.val().replace(/'/, '&#39;').replace(/"/, '&#34;').replace(/&/, '&#38;').replace(/\?/, '&#63;');
-		alert(comment)
-		$.ajax({
-		  type: "POST",
-		  url: '/includes/php/async.php?function=pb_add_comment',
-		  data: "comment="+comment+'&post_id=<?php print $product_id; ?>',
-			success: function(data, textStatus, jqXHR)
-		    {
-			    textarea.val('');
-			    $('body').trigger('click');
-		        loadedComments();
-		    },
-		    error: function (jqXHR, textStatus, errorThrown)
-		    {
-				alert('error')
-				console.log(jqXHR, textStatus, errorThrown);
-		    }
-		});
-	});
 });
-$(window).ready(function(){
-	loadedComments();
-});
-
-function loadedComments(){
-	$.ajax({
-	    url: '/includes/json/comments?q=<?php print $product_id; ?>&timeago=true',
-	    dataType: 'json',
-	    type: 'GET',
-	    error: function(xhr, error){
-		    console.debug(xhr); console.debug(error); console.log('Craw URL:', jsonURL);
-		},
-	    success: function(data) {
-		    $('#product_comemnts').empty();
-			$.each(data, function(i,json) {
-				var sql = "SELECT * FROM pb_users WHERE user_id="+json.author;
-				$.getJSON( "/includes/json/crawdb.php?get=user_data&sql="+sql, function( user_data ) {
-					$('#product_comemnts').append(
-					'<div class="col-md-12 pb-post pb-comment">'+
-				    	'<div class="pb-post-block">'+
-				            '<div class="pb-post-head">'+
-				            	'<div class="pb-menu-icon"><i class="zmdi zmdi-chevron-down"></i></div>'+
-				                '<img class="pb-post-avatar" src="'+user_data[0].avatar+'">'+
-				                '<div class="pb-post-author">'+
-				                   ' <strong><a href="/@'+user_data[0].username+'">'+user_data[0].name+'</a></strong><br>'+
-				                    '<span class="pb-post-timestamp"><i class="pb-post-timestamp-o">'+data[i].date+'</i></span>'+
-				                '</div>'+
-				            '</div>'+
-				            '<div class="pb-post-content">'+
-								''+data[i].comment+''+
-				            '</div>'+
-				        '</div>'+
-				   ' </div>'
-				   );	
-				});//end user get
-			});//end each
-	    }//end success
-	});// end agax
-}
 </script>
 </body>
 </html>
